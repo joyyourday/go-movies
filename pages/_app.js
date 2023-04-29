e9b50995dc62ad1a1ec408fe516ba9306984137f
groupincworld <118275411+joyyourday@users.noreply.github.com>
2023-04-29 08:32:59 +0530

    update 29

---
+++ pages/_app.js
@@ -0,0 +1,30 @@
+import '@styles/globals.css';
+import Footer from '../components/Footer';
+import Hamburger from '../components/Hamburger';
+import { PageTransition } from "../components/PageTransition";
+import GoogleAnalytics from "@bradgarropy/next-google-analytics";
+import Script from 'next/script';
+
+
+function Application({ Component, pageProps }) {
+ 
+
+  return ( 
+    <>
+     <div className="center">
+
+
+     
+        <PageTransition>  
+          <Hamburger />
+          <Component {...pageProps} />
+          <Footer />
+        </PageTransition>
+     </div>
+   </>
+  );
+}
+
+
+
+export default Application;
